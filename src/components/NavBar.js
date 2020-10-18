2d5c415038cdd16b6a4b2115a8fa0bc90cb7ffdc
Nicolás Olmedo <nikolmedo@gmail.com>
2020-10-18 20:33:59 -0300

    • Eliminar navbar v1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,31 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 function NavBar() {
-        // <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        //     <a className="navbar-brand" href="#">Ecommerce - ReactJS</a>
-        //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        //         <span className="navbar-toggler-icon"></span>
-        //     </button>
-
-        //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        //         <ul className="navbar-nav mr-auto">
-        //             <li className="nav-item active">
-        //                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-        //             </li>
-        //             <li className="nav-item dropdown">
-        //                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        //                 Categorias
-        //                 </a>
-        //                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-        //                     <a className="dropdown-item" href="#">Audio y video</a>
-        //                     <a className="dropdown-item" href="#">Computación</a>
-        //                     <a className="dropdown-item" href="#">Libreria</a>
-        //                     <a className="dropdown-item" href="#">Limpieza</a>
-        //                 </div>
-        //             </li>
-        //         </ul>
-        //     </div>
-        // </nav>
     return (
         <Navbar bg="primary" variant="dark">
             <Navbar.Brand href="#home">Ecommerce - ReactJS</Navbar.Brand>
